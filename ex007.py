5332707d53e20eaafd620359f5bdf595b285dc1b
Wendel Batista <wendelr90@gmail.com>
2025-10-14 23:33:58 -0300

    Exercicio-7
    "Notas e média."

---
+++ ex007.py
@@ -0,0 +1,8 @@
+# Desenvolva um porgrama que leia as duas notas de um aluno,
+# caclule e mostre a sua media
+
+nota1 = float(input('Escreva a nota do 1° Bimestre: '))
+nota2 =  float(input('EScreva a nota do 2/ Bimestre: '))
+media = (nota1 + nota2) / 2
+
+print('A soma das notas do 1° e 2° bimestre é {} \ne a media é {}'.format(nota1 + nota2, media))
