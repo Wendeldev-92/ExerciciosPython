e0a18f3840539d962ee0698c726eeeb82e7f76b9
Wendel Batista <wendelr90@gmail.com>
2025-10-14 23:32:01 -0300

    Exercicio-5
    "Operadores Aritiméticos"

---
+++ ex005.py
@@ -0,0 +1,6 @@
+# 5ª) Faça um programa que leia um numero inteiro e mostre
+# na tela o seu sucessor seu antecessor
+
+n1 = int(input('Digite um numero: '))
+print('O numero antecessor a {} é {}'.format(n1, n1 - 1))
+print('O numero sucessor a {} é {}'.format(n1, n1 + 1))
